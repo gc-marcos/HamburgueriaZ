7f068c9bdbe9fc75d5a37efa3a10a7efe348a89c
Marcos Carvalho <gcmarcoss@gmail.com>
2024-12-15 17:32:47 -0300

    Atualização e organização do repositório.

--- app/src/main/java/com/example/hamburgueriaz/MainActivity.java
+++ app/src/main/java/com/example/hamburgueriaz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hamburgueriaz;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
